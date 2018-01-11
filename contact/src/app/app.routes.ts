2f30641b4ed32dcbc3f371496fe53450e740da6d
VISTAPRINTUS\meyu <meyu@cimpress.com>
2018-01-11 18:08:05 +0800

    add default route

--- contact/src/app/app.routes.ts
+++ contact/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { EditComponent } from './edit/edit.component';
 import { DetailComponent } from './detail/detail.component';
 
 export const rootRouterConfig: Routes = [
+	{path:'',component:ListComponent},
 	{path:'list',component:ListComponent},
 	{path:'add',component:EditComponent},
 	{path:'edit/:id',component:EditComponent},
